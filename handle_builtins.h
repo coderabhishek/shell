8bd3ee11fe501a58023af16128f6243a264a45d2
coderabhishek <abhishekkmr18@gmail.com>
2016-08-26 21:53:46 +0530

    Implement . and .. support in cd

--- handle_builtins.h
+++ handle_builtins.h
@@ -10,7 +10,32 @@ int tokenize(char *str, char *tokens[50]);
 void run_cd(char *path)
 {
 	char abs_path[100];
-	getcwd(abs_path, 50);
+	abs_path[0] = '\0';
+	if(path[0]!='/')
+	{
+		getcwd(abs_path, 50);
+		int t = strlen(abs_path);
+		abs_path[t] = '/';
+		abs_path[t+1] = '\0';
+	}
+	int read_head=0, write_head=0;
+	while(abs_path[read_head]!='\0')
+	{
+		if(abs_path[read_head]=='.' && abs_path[read_head+1]!='.')
+		{
+			read_head++;
+			if(abs_path[read_head]=='/')
+				read_head++;
+		}
+		else if(abs_path[read_head]=='.')
+		{
+			while(write_head>=0 && abs_path[write_head]!='/')
+				write_head--;
+		}
+		else
+			abs_path[write_head++] = abs_path[read_head++];
+	}
+	abs_path[write_head] = '\0';
 	strcat(abs_path, path);
 	printf("absPath%s\n", abs_path);
 	chdir(abs_path);
@@ -23,6 +48,7 @@ bool handle_builtins(char cmd[])
 		run_cd(tokens[1]);
 		return false;
 	}
+	return true;
 }
 
 #endif
--- try.c
+++ try.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <signal.h>
 #include "support_routines.h"
 #include "handle_builtins.h"
@@ -14,11 +11,12 @@ void main()
 		signal(SIGINT, INThandler);
 		char cwd[50];
 		getcwd(cwd, 50);
+		cwd[0]=' ';
 		printf("%s ", cwd);
 		if(fgets(cmd, sizeof(cmd), stdin) != 0){
 			cmd[strlen(cmd)-1] = '\0';
 			bool ch = handle_builtins(cmd);
-			if(cmd[0]!='\0'){
+			if(ch && cmd[0]!='\0'){
 				char *tokens[50];
 				int tokens_size = tokenize(cmd, tokens);
 				pid_t pid = fork();
